02ec62df31b5118d68cbbaf3632f872278001a33
John Ohaeri <Johnxyn@yahoo.com>
2025-05-31 22:38:53 +0200

    final updates with project completion

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Happy developing ✨')
+
+
+const links = document.querySelectorAll('.navigation li');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+})
